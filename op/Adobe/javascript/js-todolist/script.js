5c0976561f5438064863826674be698882fc9f3d
xxx.jay <hongjae30@gmail.com>
2021-05-09 00:27:27 +0900

    todolist

--- op/Adobe/javascript/js-todolist/script.js
+++ op/Adobe/javascript/js-todolist/script.js
@@ -1,7 +1,37 @@
-const writeToDoBtn = document.querySelector('.writeToDoBtn');
-const writeToDoInput = document.querySelector('.writeToDo');
+// writeToDo 인풋
+const writeToDo = document.getElementById('writeToDo'),
+    toDoForm = document.querySelector('.toDoForm'),
+// task 리스트
+    aList = document.querySelector('.a-list'),
+    bList = document.querySelector('.b-list'),
+    cList = document.querySelector('.c-list'),
+    list = document.querySelector('.list'),
+    aColumn = document.querySelector('.a-column'),
+    bColumn = document.querySelector('.b-column'),
+    cColumn = document.querySelector('.c-column'),
+    column = document.querySelector('.column')
 
-writeToDoBtn.addEventListener('click', () => {
-    writeToDoInput.classList.toggle('active');
-    writeToDoBtn.classList.toggle('active');
-})
+// hello
+
+function handleSubmit(e){
+    e.preventDefault()
+    const currentValue = writeToDo.value
+    paintToDo(currentValue)
+}
+
+function paintToDo(text){
+    const li = document.createElement("li")
+    const delBtn = document.createElement("button")
+    delBtn.innerText = "X"
+    const span = document.createElement("span")
+    span.innerText = text
+    li.appendChild(span)
+    li.appendChild(delBtn)
+    aList.appendChild(li)
+}
+
+function init() {
+    toDoForm.addEventListener("submit", handleSubmit)
+}
+
+init()
