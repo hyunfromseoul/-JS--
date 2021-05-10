6a8e2c780e70b452acd59e6b7cdeed853bfdc65e
xxx.jay <hongjae30@gmail.com>
2021-05-11 04:54:33 +0900

    w

--- op/Adobe/javascript/js-todolist/script.js
+++ op/Adobe/javascript/js-todolist/script.js
@@ -3,12 +3,16 @@ const toDoInput = document.getElementById('writeToDo'),
     aList = document.querySelector('.a-list'),
     bList = document.querySelector('.b-list'),
     cList = document.querySelector('.c-list'),
-    abcList = document.querySelector('.list')
+    abcList = document.querySelectorAll('.list'),
+    abcContainer = document.querySelectorAll('.abc-container'),
+    item = document.querySelectorAll('.item'),
+    itemContainer = document.querySelectorAll('.item-container')
 
+    
 
-toDoBtn.addEventListener('click', writeList)
-
-function writeList(event) {
+    toDoBtn.addEventListener('click', writeItem)
+    
+function writeItem(event) {
     // form 에서 submit되어서 자동새로고침되는거 없애기
     event.preventDefault()
     // div 만들기
@@ -22,14 +26,19 @@ function writeList(event) {
         toDoDiv.appendChild(toDoLi)
         // 지우기버튼
         const deleteBtn = document.createElement('button')
-        deleteBtn.innerText = '✖️'
         deleteBtn.classList.add('delete-btn')
         toDoDiv.appendChild(deleteBtn)
-    
         aList.appendChild(toDoDiv)
+        toDoDiv.setAttribute('draggable', 'true')
         toDoInput.value = ""
+
     } else {
         alert("입력칸을 채워주세요") 
         // change toast notification to this line 
     }
 }
+
+function deleteItem(event) {
+    
+}
+
