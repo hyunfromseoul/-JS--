1c772c7ec28a93ac12df72b796640302f75835a6
xxx.jay <hongjae30@gmail.com>
2021-05-12 23:42:17 +0900

    drag n drop

--- op/Adobe/javascript/js-todolist/script.js
+++ op/Adobe/javascript/js-todolist/script.js
@@ -1,55 +1,74 @@
 const toDoInput = document.getElementById('writeToDo'),
     toDoBtn = document.querySelector('.button'),
-    aList = document.querySelector('.a-list'),
-    bList = document.querySelector('.b-list'),
-    cList = document.querySelector('.c-list'),
-    abcList = document.querySelectorAll('.list'),
+    aContainer = document.querySelector('.a-container'),
+    bContainer = document.querySelector('.b-container'),
+    cContainer = document.querySelector('.c-container'),
     abcContainer = document.querySelectorAll('.abc-container'),
     item = document.querySelectorAll('.item'),
-    itemContainer = document.querySelectorAll('.item-container')
+    container = document.querySelector('.container');
 
-    itemContainer.forEach((items) => {
-        items.addEventListener('dragstart', () => {
-            items.classList.add('dragging')
-            console.log('add')
+
+container.addEventListener("mousedown", () => {
+    // console.log('container가 클릭이 됌')
+    // console.log(container)
+
+    let itemContainer = document.querySelectorAll('.item-container');
+    
+    itemContainer.forEach((item) => {
+        item.addEventListener("dragstart", () => {
+            item.classList.add('dragging')
         })
 
-        items.addEventListener('dragend', () => {
-            items.classList.remove('dragging')
-            console.log('remove')
+        item.addEventListener("dragend", () => {
+            item.classList.remove('dragging')
         })
-    });
-    
+    })
 
-    toDoBtn.addEventListener('click', writeItem)
-    
+    abcContainer.forEach(con => {
+        con.addEventListener('dragover', e => {
+            e.preventDefault()
+            const draggable = document.querySelector('.dragging')
+            con.appendChild(draggable)
+        })
+
+        con.addEventListener('drop', e => {
+        })
+    })
+})
+
+
+
+
+// 이벤트리스너
+toDoBtn.addEventListener('click', writeItem)
+
+
+
+// 인풋값에서 submit된 값을 투두에 옮기기
 function writeItem(event) {
     // form 에서 submit되어서 자동새로고침되는거 없애기
     event.preventDefault()
+
     // div 만들기
     if (toDoInput.value !== "") {
         const toDoDiv = document.createElement("div")
         toDoDiv.classList.add("item-container")
         // li만들기
-        const toDoLi = document.createElement("li")
-        toDoLi.innerText = toDoInput.value
-        toDoLi.classList.add('item')
-        toDoDiv.appendChild(toDoLi)
+        // const toDoSpan = document.createElement("span")
+        // toDoSpan.innerText = toDoInput.value
+        // toDoSpan.classList.add('item')
+        // toDoDiv.appendChild(toDoSpan)
+        toDoDiv.innerText = toDoInput.value
         // 지우기버튼
         const deleteBtn = document.createElement('button')
         deleteBtn.classList.add('delete-btn')
         toDoDiv.appendChild(deleteBtn)
-        aList.appendChild(toDoDiv)
+        aContainer.appendChild(toDoDiv)
         toDoDiv.setAttribute('draggable', 'true')
         toDoInput.value = ""
 
     } else {
         alert("입력칸을 채워주세요") 
-        // change toast notification to this line 
+        // 토스트 알람으로 바꾸기
     }
 }
-
-function deleteItem(event) {
-    
-}
-
